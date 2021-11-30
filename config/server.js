c28b2baf4df36abbf16637dfcee66942e0f5700b
Alex Garcia <alex.g9415@gmail.com>
2021-11-29 21:21:09 -0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f6d4709a38ad6f29893abeabce52ead'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6c2a3f52-bcf7-40cc-a9e3-9df3b6d30275'
+};
